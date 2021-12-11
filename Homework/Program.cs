d05125135f9e2aa68fe138b4e6f93cbb0ba01440
SebiQA <sebi_filip07@yahoo.com>
2021-12-12 00:00:33 +0200

    Arithmetic calculator

---
+++ Homework/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    { 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            if (args.Length == 3)
+            {
+
+                float a = float.Parse(args[0]);
+
+                float b = float.Parse(args[2]);
+
+                string op = args[1];
+
+                float result;
+
+                switch (op)
+                {
+                    case "*":
+                        {
+                            result = a * b;
+                            Console.WriteLine("Result i s" + result);
+                            break;
+                        }
+
+                    case "+":
+                        {
+                            result = a + b;
+                            Console.WriteLine("Result is " + result);
+                            break;
+                        }
+
+                    case "-":
+                        {
+                            result = a - b;
+                            Console.WriteLine("Result is " + result);
+                            break;
+                        }
+
+                    case "/":
+                        {
+                            result = a / b;
+                            Console.WriteLine("Result is " + result);
+                            break;
+                        }
+
+                    case "%":
+                        {
+                            result = a % b;
+                            Console.WriteLine("Result is " + result);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid inputs");
+                            break;
+                        }
+                }
+                        
+            }
+            else
+            {
+
+                Console.WriteLine("3 args are needed!");
+
+            }
+
+        }
+    }
+}
+

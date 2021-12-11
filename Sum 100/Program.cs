241194e25139164d8a3d48159c4d4f28abce83e6
SebiQA <sebi_filip07@yahoo.com>
2021-12-12 00:21:44 +0200

    Sum 100

---
+++ Sum 100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sum_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Professor!");
+
+            int i = 1;
+            int sum = 0;
+
+            while (i <= 100)
+            {
+                sum = sum + i;
+                i++;
+            }
+
+            Console.WriteLine("Suma primelor 100 numere naturale > 0 este " + sum);
+                
+        }
+    }
+}

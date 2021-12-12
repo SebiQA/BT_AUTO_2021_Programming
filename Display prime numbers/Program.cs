1b676fb1877e5c116790046d707d160a4c9198c8
SebiQA <sebi_filip07@yahoo.com>
2021-12-12 20:18:20 +0200

    Display prime numbers

---
+++ Display prime numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Display_prime_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Professor!");
+
+            int limita = 40;
+            int no;
+            int i;
+            int count;
+
+            for (no = 2; no <= limita; no++)
+            {
+                count = 0;
+                for (i = 2; i <= no; i++)
+                {
+                    if (no % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 1)
+                {
+                    Console.WriteLine(no);
+                }
+            }
+
+        }
+    }
+}

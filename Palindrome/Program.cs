05a86980c42f2e9ab01dd51131eeee80b4b35bac
SebiQA <sebi_filip07@yahoo.com>
2021-12-17 19:03:15 +0200

    Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Professor!");
+
+
+            int p, r, sum = 0, inv;
+            Console.Write("The number to be checked as palindrome is: ");
+            p = int.Parse(Console.ReadLine());
+            inv = p;
+            while (p > 0)
+            {
+                r = p % 10;
+                sum = (sum * 10) + r;
+                p = p / 10;
+            }
+            if (inv == sum)
+            {
+                Console.Write("Number is palindrome.");
+            }
+            else
+            {
+                Console.Write("Number is not palindrome");
+            }
+        }
+    }
+}
